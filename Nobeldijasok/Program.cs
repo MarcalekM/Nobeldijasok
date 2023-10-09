46299b5c1c2a501338fd04a0a3f46bd0eaa2b880
Máté Marcalek <matemarcalek@gmail.com>
2023-10-09 09:31:34 +0200

    5. feladat elkezdve

--- Nobeldijasok/Program.cs
+++ Nobeldijasok/Program.cs
@@ -16,6 +16,7 @@ namespace Nobeldijasok
                 encoding: System.Text.Encoding.UTF8);
             _ = sr.ReadLine();
             while (!sr.EndOfStream) dijazottak.Add(new Dijazott(sr.ReadLine()));
+
             Console.WriteLine("3. feladat:");
             Console.WriteLine($"\tÖsszesen {dijazottak.Count} db díjazott adatai találhatóak meg");
 
@@ -24,6 +25,16 @@ namespace Nobeldijasok
                 .OrderBy(d => d.Ev)
                 .Last();
             Console.WriteLine($"\tAz utolsó díjátadó éve: {f4.Ev}");
+
+            Console.WriteLine("5. feladat:");
+            Console.Write("Adjon meg egy országkódot:  ");
+            string kod = Console.ReadLine();
+            var f5 = dijazottak.GroupBy(d => d.Orszagkod == kod);
+            foreach (var f in f5)
+            {
+                Console.WriteLine(f);
+            }
+
         }
     }
 }
