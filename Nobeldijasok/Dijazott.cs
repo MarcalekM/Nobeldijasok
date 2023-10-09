e301ff3ca04fd8fa1cc46503c9cd29af76225af0
Máté Marcalek <matemarcalek@gmail.com>
2023-10-09 09:14:32 +0200

    Add project files.

---
+++ Nobeldijasok/Dijazott.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobeldijasok
+{
+    class Dijazott
+    {
+        public int Ev { get; set; }
+        public string Nev { get; set; }
+        public string SzulHal { get; set; }
+        public string Orszagkod { get; set; }
+
+        public Dijazott(string sor)
+        {
+            var v = sor.Split(';');
+            Ev = int.Parse(v[0]);
+            Nev = v[1];
+            SzulHal = v[2];
+            Orszagkod = v[3];
+        }
+    }
+}
---
+++ Nobeldijasok/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Encodings;
+using System.Linq;
+
+namespace Nobeldijasok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dijazottak = new List<Dijazott>();
+            var sr = new StreamReader(
+                path: @"..\..\..\src\orvosi_nobeldijak.txt",
+                encoding: System.Text.Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) dijazottak.Add(new Dijazott(sr.ReadLine()));
+            Console.WriteLine("3. feladat:");
+            Console.WriteLine($"\tÖsszesen {dijazottak.Count} db díjazott adatai találhatóak meg");
+
+            Console.WriteLine("4.feladat:");
+
+        }
+    }
+}
