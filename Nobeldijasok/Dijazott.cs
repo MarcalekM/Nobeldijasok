468851628ee6b6ed8ca6094ebf08f07cfc6ef8bb
Máté Marcalek <matemarcalek@gmail.com>
2023-10-10 08:22:19 +0200

    Osztály bővítés

--- Nobeldijasok/Dijazott.cs
+++ Nobeldijasok/Dijazott.cs
@@ -11,6 +11,7 @@ namespace Nobeldijasok
         public int Ev { get; set; }
         public string Nev { get; set; }
         public string SzulHal { get; set; }
+        public (int Szuletes, int? Halal) SzH { get; set; }
         public string Orszagkod { get; set; }
 
         public Dijazott(string sor)
@@ -19,6 +20,9 @@ namespace Nobeldijasok
             Ev = int.Parse(v[0]);
             Nev = v[1];
             SzulHal = v[2];
+            var szhv = v[2].Split('-');
+            int? h = szhv[1].Length == 0 ? null : int.Parse(szhv[1]);
+            SzH = (int.Parse(szhv[0]), h);
             Orszagkod = v[3];
         }
     }
